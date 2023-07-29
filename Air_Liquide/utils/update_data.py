2ed62d236df1ab85b19f823a60fd63b648d9a973
MarcAureleAgbo <dastafr@gmail.com>
2023-07-29 22:32:04 +0200

    yes

--- Air_Liquide/utils/update_data.py
+++ Air_Liquide/utils/update_data.py
@@ -1,54 +1,72 @@
 import psycopg2
-import yfinance as yf
-from datetime import date, timedelta
+import pandas as pd
+from datetime import datetime, timedelta
 
-# Établir une connexion à la base de données
-conn = psycopg2.connect(
-    host="localhost",
-    port="5432",
-    database="AirLiquide",
-    user="postgres"
-)
-# Créer un curseur pour exécuter les requêtes
-cur = conn.cursor()
 
-def update_data(date):
-    # Créer un objet Ticker pour AirLiquide
-    ticker = yf.Ticker("AI.PA")
+def update_data():
+    """
+    S'assurer que le fichier AI.PA.csv est à jour. A cette étape, le remplacer manuellement. TODO: Trouver une solution de le remplacer automatiquement
+    """
+    # Établir une connexion à la base de données
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="AirLiquide",
+        user="postgres"
+    )
+    # Créer un curseur pour exécuter les requêtes
+    cur = conn.cursor()
 
-    # Obtenir les données historiques jusqu'à la date spécifiée
-    history = ticker.history(start=date, end=date)
+    # dernière date dans en bdd
+    cur.execute("SELECT max(date) FROM raw_data")
+    derniere_date_bdd = cur.fetchone()[0]
 
-    if history.empty:
-        print("Aucune donnée disponible pour la date spécifiée.")
-        return
+    # dernière date renseignée en ligne
+    df = pd.read_csv('/home/maa/Documents/GitHub/works/Air_Liquide/data/AI.PA.csv', parse_dates=['Date'])
+    df = df.sort_values('Date', ascending=False).reset_index(drop=True)[['Date','Low']]
+    derniere_date_a_jour = df.Date[0].date()
 
-    # Récupérer la valeur "Low" et la date correspondante
-    low_value = history["Low"].iloc[0]
-    date = history.index[0].date()
+    # Dates à mettre à jour
+    dates_maj = []
+    date_origine = derniere_date_bdd
+    while derniere_date_bdd <= derniere_date_a_jour:
+        if derniere_date_bdd != date_origine:
+            dates_maj.append(derniere_date_bdd.strftime('%Y-%m-%d'))
+        derniere_date_bdd += timedelta(days=1)
 
-    # Afficher la date et la valeur "Low" d'AirLiquide
-    print("Date :", date)
-    print("Valeur Low d'AirLiquide :", low_value)
+    print("Démarrage de la mise à jour:\n")
 
-    # Vérifier si la date existe déjà dans la table
-    cur.execute("SELECT date FROM raw_data WHERE date = %s", (date,))
-    existing_date = cur.fetchone()
-
-    if existing_date:
-        # La date existe déjà dans la table
-        print("La date existe déjà dans la base de données.")
+    if dates_maj != []:
+        
+        for d in dates_maj:
+            print(str(d))
+            low = df[df.Date == d]['Low'].iloc[0]
+            cur.execute("INSERT INTO raw_data (date, low) VALUES (%s, %s)", (d, low))
+        print("Done")
     else:
-        # La date n'existe pas dans la table, ajouter les données
-        cur.execute("INSERT INTO raw_data (date, low) VALUES (%s, %s)", (date, low_value))
-        print("Données ajoutées avec succès.")
-
+        print("Aucune date manquante!")
     # Valider la transaction
-        conn.commit()
+    conn.commit()
+    
+    # Fermer le curseur et la connexion à la base de données
+    cur.close()
+    conn.close()
 
-    return "raw_data updated to " + str(date)
+    return "raw_data updated to " + str(derniere_date_a_jour)
+
+   
 
 def update_pred(nouvelle_date,pred):
+    # Établir une connexion à la base de données
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="AirLiquide",
+        user="postgres"
+    )
+    # Créer un curseur pour exécuter les requêtes
+    cur = conn.cursor()
+    
     # Vérifier si le jour existe déjà dans la table
     cur.execute("SELECT date FROM predictions WHERE date = %s", (nouvelle_date,))
     existing_date = cur.fetchone()
@@ -62,40 +80,19 @@ def update_pred(nouvelle_date,pred):
         print("Données ajoutées avec succès.")
     # Valider la transaction
         conn.commit()
+        
+    # Fermer le curseur et la connexion à la base de données
+    cur.close()
+    conn.close()
+    
     return "predictions updated to "+str(nouvelle_date)
 
 
 
 if __name__ == "__main__":
     
-    # dates à mettre à jour
-    cur.execute("SELECT max(date) FROM raw_data")
-    derniere_date = cur.fetchone()[0]
-    aujourd_hui = date.today()
-    liste_dates = []
-    delta = timedelta(days=1)
-    current_date = derniere_date
-    while current_date <= aujourd_hui:
-        liste_dates.append(current_date.strftime("%Y-%m-%d"))
-        current_date += delta
-
-    print("liste des dates à MAJ: ",liste_dates)
-
-    
-    
-    
     # Mise à jour des données de la base
-#    for d in liste_dates:
-#        update_data(date=d)
+    update_data()
+    
     # maj des valeurs prédites
     # update_pred()
-
-
-
-
-
-
-
-    # Fermer le curseur et la connexion à la base de données
-    cur.close()
-    conn.close()
